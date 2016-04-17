66ba946389c20d5c9420f69eb0fdb2e9002c479c
Andrey Viktorov <andrey.viktorov@gmail.com>
2016-04-17 16:02:15 +0600

    You died

---
+++ Assets/Game/Scripts/Game/GameController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameController : MonoSingleton<GameController> {
+	
+	public void Restart() {
+		SceneManager.LoadScene("main");
+	}
+}
--- Assets/Game/Scripts/Game/UIController.cs
+++ Assets/Game/Scripts/Game/UIController.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class UIController : MonoSingleton<UIController> {
 	
 	private void OnGUI() {
-		
+		// TODO: get rid of this
 		GUILayout.BeginVertical("box");
 		GUILayout.Label("Controls:");
 		GUILayout.Label("W / S / A / D: move");
